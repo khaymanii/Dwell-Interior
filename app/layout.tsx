8162f40893039d58294a2c9ede2581dec13ba9fb
khaymanii <kennethakpo61@gmail.com>
2024-07-06 17:45:44 +0100

    created navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" className="">
       <body className={besley.className}>
         <Provider>{children}</Provider>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        customGreen: "#217575",
+        customGreen1: "#F0F5F5",
       },
       fontFamily: {
         besley: ["Besley", "serif"], // Define the Besley font
